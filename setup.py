4bf5ecadb7d2d4003367eb66290188b602c2563a
Zhifeng Jiang <samuelgong2017@gmail.com>
2025-12-03 12:36:11 +0800

    A

--- setup.py
+++ setup.py
@@ -25,6 +25,16 @@ setup(
     version="0.1.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    package_data={
+            # 这里的 key 要写“包含 init_files 的那个 package 名字”
+            # 你的路径是 site-packages/libero/libero/init_files
+            # 一般对应包名就是 "libero.libero"
+            "libero.libero": [
+                "init_files/*",
+                "init_files/*/*",      # 一层子目录
+                "init_files/*/*/*",    # 再多一层（保险一点）
+            ],
+        },
     entry_points={
         "console_scripts": [
             "lifelong.main=libero.lifelong.main:main",
